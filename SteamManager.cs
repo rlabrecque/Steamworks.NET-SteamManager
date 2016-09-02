5148c3a84a7e3da775565a71c8730a51857e30a1
Fred Sauer <fredsa@gmail.com>
2016-09-01 21:53:26 -0700

    clarify init failure messages

--- SteamManager.cs
+++ SteamManager.cs
@@ -82,11 +82,11 @@ class SteamManager : MonoBehaviour {
 
 		// Initialize the SteamAPI, if Init() returns false this can happen for many reasons.
 		// Some examples include:
-		// Steam Client is not running.
-		// Launching from outside of steam without a steam_appid.txt file in place.
-		// Running under a different OS User or Access level (for example running "as administrator")
-		// Ensure that you own a license for the AppId on your active Steam account
-		// If your AppId is not completely set up. Either in Release State: Unavailable, or if it's missing default packages.
+		// - Steam Client is not running.
+		// - Launching from outside of steam without a steam_appid.txt file in place.
+		// - Running under a different OS User or Access level (for example running "as administrator")
+		// - The currently logged in Steam account doesn't own a license for the AppId listed in steam_appid.txt
+		// - Your AppId is not completely set up, i.e. in Release State: Unavailable, or if it's missing default packages
 		// Valve's documentation for this is located here:
 		// https://partner.steamgames.com/documentation/getting_started
 		// https://partner.steamgames.com/documentation/example // Under: Common Build Problems
